ae7f85126da484ce835b2a1b6747620b7c4cdfb4
samueleblanc <86082064+samueleblanc@users.noreply.github.com>
2021-08-24 19:06:11 -0400

    Solving RT

--- Javascript/image1.js
+++ Javascript/image1.js
@@ -1,10 +1,10 @@
 // Get the modal
-let modal = document.getElementById("myModal");
+let modal = document.getElementById("myModal1");
 
 // Get the image and insert it inside the modal - use its "alt" text as a caption
-let img = document.getElementById("myImg");
+let img = document.getElementById("myImg1");
 let modalImg = document.getElementById("img01");
-let captionText = document.getElementById("caption");
+let captionText = document.getElementById("caption1");
 img.onclick = function(){
   modal.style.display = "block";
   modalImg.src = this.src;
